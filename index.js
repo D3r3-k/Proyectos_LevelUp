af02d484436447be56181d976c962d6e4b67edb2
D3r3-k <Derek16ib@gmail.com>
2022-11-05 23:12:36 -0600

    Proyecto 1 - Level up

---
+++ index.js
@@ -0,0 +1,35 @@
+var obj1 = {
+    b: 2,
+    c: 3,
+    a: 1
+    }
+var obj2 = {
+    a: "Apple",
+    c: "Google",
+    b: "Microsoft"
+    }
+var obj3 = {
+    key1: true,
+    key2: false,
+    key3: undefined,
+    }
+
+    function separar(obj) {
+        var ordenado = Object.entries(obj).sort()
+        var keys = []
+        var values = []
+        for (const key in ordenado) {
+            keys.push(ordenado[key][0])
+            values.push(ordenado[key][1])
+        }
+        console.log("\n> Original");
+        console.log(obj);
+        console.log("> Separado  y Ordenado");
+        console.log(keys);
+        console.log(values);
+        console.log();
+    }
+
+    separar(obj1)
+    separar(obj2)
+    separar(obj3)
